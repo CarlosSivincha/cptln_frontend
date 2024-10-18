77680fd46e68e42652668ab2ce19665948b26768
CarlosSivincha <carlossivincha251413@gmail.com>
2024-10-17 22:34:17 -0500

    first commit

--- src/Api/Devo.js
+++ src/Api/Devo.js
@@ -1,12 +1,4 @@
 import axios from "./axios";
 
-// Función para registrar un devocional
-export const registrarDevo = async (devo) => {
-    try {
-        const response = await axios.post("devocionales", devo);
-        return response.data; // Retorna los datos de la respuesta
-    } catch (error) {
-        console.error("Error al registrar el devocional:", error); // Manejo de errores
-        throw error; // Vuelve a lanzar el error para que se maneje en el componente
-    }
-};
+export const registrarDevo = devo => axios.post("devocionales",devo)
+
--- src/pages/Admin/pages/DevoAdmin/index.jsx
+++ src/pages/Admin/pages/DevoAdmin/index.jsx
@@ -1,87 +1,92 @@
 import { useState } from "react";
+import { format } from "date-fns"; // Asegúrate de tener esta importación si vas a formatear la fecha
+import { es } from "date-fns/locale"; // Importa la localización si es necesario
 import Header from "@/pages/client/components/Header"; // Asegúrate de que esta ruta sea correcta
 import { registrarDevo } from "../../../../Api/Devo";
 
+
 const YourFormComponent = () => {
-    const [formData, setFormData] = useState({
-        titulo: '',
-        fecha: '',
-        versiculo: '',
-        parrafo: '',
-        imagen: null
-    });
 
-    // Simular el envío del formulario
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-    
-        // Crear un objeto FormData para enviar todos los datos, incluyendo la imagen
-        const dataToSend = new FormData();
-        dataToSend.append('titulo', formData.titulo);
-        dataToSend.append('fecha', formData.fecha); // Envía la fecha tal como está en el formulario
-        dataToSend.append('versiculo', formData.versiculo);
-        dataToSend.append('parrafo', formData.parrafo);
-        
-        // Verifica que hay una imagen antes de agregarla
-        if (formData.imagen) {
-            dataToSend.append('imagen', formData.imagen);
-            console.log("Imagen seleccionada:", formData.imagen); // Verifica que se ha seleccionado una imagen
-        } else {
-            console.warn("No se ha seleccionado ninguna imagen."); // Mensaje de advertencia
-        }
-    
+    const [imagen, setImagen] = useState(null);
+    const [titulo, setTitulo] = useState("")
+    const [versiculo, setVersiculo] = useState("")
+    const [parrafo, setParrafo] = useState("")
+    const [fecha, setFecha] = useState("")
+
+    const handleImagen = (event) => {
+        setImagen(event.target.files[0])
+    }
+    const handleTitulo = (event) => {
+        setTitulo(event.target.value)
+    }
+    const handleVersiculo = (event) => {
+        setVersiculo(event.target.value)
+    }
+    const handleParrafo = (event) => {
+        setParrafo(event.target.value)
+    }
+    const handleFecha = (event) => {
+        setFecha(event.target.value)
+    }
+
+    const subirdevo = async (event) => {
+        event.preventDefault()
+        const formData = new FormData();
+        formData.append('imagen', imagen);
+        formData.append('titulo', titulo);
+        formData.append('versiculo', versiculo);
+        formData.append('parrafo', parrafo);
+        formData.append('fecha', fecha);
         try {
-            await registrarDevo(dataToSend); // Envía dataToSend a la API
-            console.log("Devocional registrado con éxito"); // Mensaje de éxito
+            const respuesta = await registrarDevo(formData)
+            console.log(respuesta)
         } catch (error) {
-            console.error("Error al registrar el devocional:", error); // Manejo de errores
+            console.log(error)
         }
-    };
-
+    }
 
     return (
         <>
             <Header color="bg-l_color_r-600" title={'Crear Devocional'} />
             <div className="max-w-2xl p-5 mx-auto">
-                <h2 className="mb-6 text-3xl font-bold">Escribe el Devocional</h2>
-                <form onSubmit={handleSubmit} className="space-y-4">
+                <h2 className="mb-6 text-3xl font-bold">Escribe el Devocional </h2>
+                <form onSubmit={subirdevo} className="space-y-4">
                     <input
                         type="text"
-                        name="titulo"
-                        value={formData.titulo}
-                        onChange={(e) => setFormData({ ...formData, titulo: e.target.value })}
-                        placeholder="Titulo"
+                        name="title"
+                        value={titulo}
+                        onChange={handleTitulo}
+                        placeholder="Título"
                         className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-l_color_y-600"
                     />
                     <input
                         type="date"
                         name="fecha"
-                        value={formData.fecha}
-                        onChange={(e) => setFormData({ ...formData, fecha: e.target.value })}
+                        value={fecha}
+                        onChange={handleFecha}
                         className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-l_color_y-600"
                     />
                     <input
                         type="text"
                         name="versiculo"
-                        value={formData.versiculo}
-                        onChange={(e) => setFormData({ ...formData, versiculo: e.target.value })}
+                        value={versiculo}
+                        onChange={handleVersiculo}
                         placeholder="Versículo"
                         className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-l_color_y-600"
                     />
                     <textarea
                         name="parrafo"
-                        value={formData.parrafo}
-                        onChange={(e) => setFormData({ ...formData, parrafo: e.target.value })}
-                        placeholder="Parrafo"
+                        value={parrafo}
+                        onChange={handleParrafo}
+                        placeholder="Contenido"
                         className="w-full h-40 p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-l_color_y-600"
                     />
                     <input
                         type="file"
                         name="imagen"
-                        onChange={(e) => setFormData({ ...formData, imagen: e.target.files[0] })}
+                        onChange={handleImagen}
                         className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-l_color_y-600"
                     />
-                
                     <button type="submit" className="w-full p-3 font-semibold text-white transition duration-200 rounded-md bg-l_color_y-600 hover:bg-l_color_y-700">
                         Enviar
                     </button>
@@ -92,3 +97,4 @@ const YourFormComponent = () => {
 };
 
 export default YourFormComponent;
+
