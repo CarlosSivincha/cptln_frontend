8a5ce9f0ba891718aba1f27fb743d636a95618f4
mvillanueva24 <mariorvillanueva24@gmail.com>
2024-10-22 14:58:08 -0500

    push

--- src/pages/admin/Administracion/index.jsx
+++ src/pages/admin/Administracion/index.jsx
@@ -5,6 +5,7 @@ import { MdOutlineEvent } from 'react-icons/md';
 import { ImNewspaper } from "react-icons/im";
 import { FaPrayingHands } from "react-icons/fa";
 import { GiOpenBook } from "react-icons/gi";
+import { Link } from "react-router-dom";
 
 const Administracion = () => {
     
@@ -19,43 +20,41 @@ const Administracion = () => {
                     </span>
                 </div>
                 <div className="flex items-center font-bold text-white">
-                    Mario Uwu
+                    Mario Usuario Admin
                 </div>
             </nav>
             <div className="flex">
                 {/* Menú lateral */}
                 <div
-                    className="fixed bottom-0 left-0 w-24 transition-all duration-300 bg-white top-20 hover:w-96 group"
-                >
+                    className="fixed bottom-0 left-0 w-24 transition-all duration-300 bg-white top-20 hover:w-96 group"  >
                     <ul>
                         <li className="flex items-center justify-center w-full py-4 transition-all duration-300 cursor-pointer group hover:bg-slate-400">
-                            <span className="hidden ml-4 text-xl group-hover:inline-block">
+                            <Link to="/admin/tablaevento" className="hidden ml-4 text-xl group-hover:inline-block ">
                                 Eventos
-                            </span>
+                            </Link>
                             <MdOutlineEvent className="block group-hover:hidden size-8" />
                         </li>
                         <li className="flex items-center justify-center w-full py-4 transition-all duration-300 cursor-pointer group hover:bg-slate-400">
-                            <span className="hidden ml-4 text-xl group-hover:inline-block">
+                            <Link to="/admin/tablanews" className="hidden ml-4 text-xl group-hover:inline-block ">
                                 Noticias
-                            </span>
+                            </Link>
                             <ImNewspaper className="block group-hover:hidden size-8" />
                         </li>
                         <li className="flex items-center justify-center w-full py-4 transition-all duration-300 cursor-pointer group hover:bg-slate-400">
-                            <span className="hidden ml-4 text-xl group-hover:inline-block">
+                            <Link to="/admin/tablaevento" className="hidden ml-4 text-xl group-hover:inline-block ">
                                 Oraciones
-                            </span>
+                            </Link>
                             <FaPrayingHands className="block group-hover:hidden size-8" />
                         </li>
                         <li className="flex items-center justify-center w-full py-4 transition-all duration-300 cursor-pointer group hover:bg-slate-400">
-                            
-                            <span className="hidden ml-4 text-xl group-hover:inline-block">
+                            <Link to="/admin/tabladevocional" className="hidden ml-4 text-xl group-hover:inline-block ">
                                 Devocionales
-                            </span>
+                            </Link>
                             <GiOpenBook className="block group-hover:hidden size-8" />
                         </li>
                     </ul>
                 </div>
-                
+
                 {/* Contenido principal */}
                 <div
                     className="flex flex-col w-full overflow-y-auto max-h-[calc(100vh-5rem)] transition-all duration-300 ml-20"
