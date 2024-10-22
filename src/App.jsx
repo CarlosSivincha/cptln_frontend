2b3fa715356271bc2574e219afe7e39466111c15
mvillanueva24 <mariorvillanueva24@gmail.com>
2024-10-22 14:55:18 -0500

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { StrictMode, lazy, Suspense } from "react";
-import  PageLoader from "./pages/client/components/Loaders/PageLoader";
+import PageLoader from "./pages/client/components/Loaders/PageLoader";
 import CursosBiblico from "./pages/client/pages/CursosBiblicos";
 import { AuthProvider } from "./context/Usuario_context";
 
@@ -8,90 +8,130 @@ const AboutUs = lazy(() => import("@/pages/client/pages/AboutUs"));
 const Programas = lazy(() => import("@/pages/client/pages/Programs"));
 const Niños = lazy(() => import("@/pages/client/pages/Programs/Niños"));
 const Familia = lazy(() => import("@/pages/client/pages/Programs/Familia"));
-const EquipandoSantos = lazy(() => import("@/pages/client/pages/Programs/EquipandoSantos"));
+const EquipandoSantos = lazy(() =>
+  import("@/pages/client/pages/Programs/EquipandoSantos")
+);
 const Home = lazy(() => import("@/pages/client/pages/Home"));
 const Ebooks = lazy(() => import("@/pages/client/pages/Ebooks"));
 const Noticia = lazy(() => import("@/pages/client/pages/Noticias"));
 const Oracion = lazy(() => import("@/pages/client/pages/Oracion"));
 const Devocional = lazy(() => import("@/pages/client/pages/Devocional"));
-const DevocionalesDiarios = lazy(() => import("@/pages/client/pages/DevocionalesDiarios"));
+const DevocionalesDiarios = lazy(() =>
+  import("@/pages/client/pages/DevocionalesDiarios")
+);
 const Navbar = lazy(() => import("@/pages/client/components/Navbar"));
 const Fotter = lazy(() => import("@/pages/client/components/Fotter"));
 
 const Donate = lazy(() => import("@/pages/client/pages/Donate"));
-const CursosBiblicos = lazy(() => import("@/pages/client/pages/CursosBiblicos"));
+const CursosBiblicos = lazy(() =>
+  import("@/pages/client/pages/CursosBiblicos")
+);
 const Joel = lazy(() => import("@/pages/client/pages/Program/Joel"));
 const Pasi = lazy(() => import("@/pages/client/pages/Program/Pasi"));
-const JuntosComunidad = lazy(() => import("@/pages/client/pages/Program/JuntosComunidad"));
+const JuntosComunidad = lazy(() =>
+  import("@/pages/client/pages/Program/JuntosComunidad")
+);
 const Contact = lazy(() => import("@/pages/client/pages/Contact"));
 const NewsEvents = lazy(() => import("@/pages/client/pages/NewsEvents"));
-const NotFound = lazy(() => import("@/pages/client/pages/ExtraPages/NotFound"))
+const NotFound = lazy(() => import("@/pages/client/pages/ExtraPages/NotFound"));
 
 // Admin const
 const LoginAdmin = lazy(() => import("@/pages/admin/LoginAdmin"));
 const RegisterAdmin = lazy(() => import("@/pages/admin/RegisterAdmin"));
 const DevoAd = lazy(() => import("@/pages/admin/DevoAdmin"));
-const NewsAdmin = lazy(() => import("@/pages/admin/NewsAdmin"))
-const EventsAdmin = lazy(() => import("@/pages/admin/EventsAdmin"))
+const NewsAdmin = lazy(() => import("@/pages/admin/NewsAdmin"));
+const EventsAdmin = lazy(() => import("@/pages/admin/EventsAdmin"));
 // const Navbar = lazy(() => import("@/pages/client/components/Navbar"));
-const Administracion = lazy(() => import("@/pages/admin/Administracion"))
-const TablaEvento = lazy(() => import("@/pages/admin/TablaEvento"))
-const TablaDevocional = lazy(() => import("@/pages/admin/TablaDevocional"))
-const TablaNews = lazy(() => import("@/pages/admin/TablaNews"))
+const Administracion = lazy(() => import("@/pages/admin/Administracion"));
+const TablaEvento = lazy(() => import("@/pages/admin/TablaEvento"));
+const TablaDevocional = lazy(() => import("@/pages/admin/TablaDevocional"));
+const TablaNews = lazy(() => import("@/pages/admin/TablaNews"));
 
 const App = () => {
   return (
     <>
       <StrictMode>
-      <AuthProvider>
-        <div className="bg-[#EAE9E5]">
-        <Suspense fallback={<PageLoader/>}>
-          <Navbar color="bg-l_color_r-600"/>          
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<Home/>} />
-              <Route path="/aboutus" element={<AboutUs/>} />
-              <Route path="/programas" element={<Programas/>}/>
-              <Route path="/programas/niños-adolescentes" element={<Niños/>}/>
-                <Route path="/programas/niños-adolescentes/joel" element={<Joel/>}/>
-                <Route path="/programas/niños-adolescentes/pasi" element={<Pasi/>}/>
-                <Route path="/programas/niños-adolescentes/juntos-en-comunidad" element={<JuntosComunidad/>}/>
+        <AuthProvider>
+          <div className="bg-[#EAE9E5]">
+            <Suspense fallback={<PageLoader />}>
+              {/* <Navbar color="bg-l_color_r-600"/>           */}
+              <BrowserRouter>
+                <Routes>
+                  <Route path="/" element={<Navbar color="bg-l_color_r-600" />}>
+                    <Route index element={<Home />} />
+                    <Route path="aboutus" element={<AboutUs />} />
+                    <Route path="programas" element={<Programas />} />
+                    <Route
+                      path="programas/niños-adolescentes"
+                      element={<Niños />}
+                    />
+                    <Route
+                      path="programas/niños-adolescentes/joel"
+                      element={<Joel />}
+                    />
+                    <Route
+                      path="programas/niños-adolescentes/pasi"
+                      element={<Pasi />}
+                    />
+                    <Route
+                      path="programas/niños-adolescentes/juntos-en-comunidad"
+                      element={<JuntosComunidad />}
+                    />
 
-              <Route path="/programas/familia" element={<Familia/>}/>
-              <Route path="/programas/equipando-los-santos" element={<Familia/>}/>
-                {/* <Route path="/programas/joel" element={<Joel/>}/>
+                    <Route path="programas/familia" element={<Familia />} />
+                    <Route
+                      path="programas/equipando-los-santos"
+                      element={<Familia />}
+                    />
+                    {/* <Route path="/programas/joel" element={<Joel/>}/>
                 <Route path="/programas/pasi" element={<Joel/>}/>
                 <Route path="/programas/juntos-en-comunidad" element={<Joel/>}/> */}
 
-              <Route path="/noticia/:id" element={<Noticia/>}/>
-              <Route path="/recursos/ebooks" element={<Ebooks/>} />
-              <Route path="/recursos/devocional-diario" element={<Devocional/>} />
-              <Route path="/recursos/cursos-biblicos" element={<CursosBiblicos/>} />
-              <Route path="/noticias-y-eventos" element={<NewsEvents/>} />
-              <Route path="/donate" element={<Donate/>}   />
-              <Route path="/contactanos" element={<Contact/>} />
-              <Route path="/oracion" element={<Oracion />} />
-              <Route path="/recursos/devocionales" element={<DevocionalesDiarios />} />
-              <Route path="*" element={<NotFound />} />
-              <Route path="/admin" element={<Administracion/>}>
-                <Route path="register" element={<RegisterAdmin />} />
-                <Route path="devoAd" element={<DevoAd />} />
-                <Route path="login" element={<LoginAdmin />} />
-                <Route path="newsad" element={<NewsAdmin />} />
-                <Route path="eventad" element={<EventsAdmin />} />
-                <Route path="tablaevento" element={<TablaEvento />} />
-                <Route path="tablaevento/:id" element={<EventsAdmin />} />
-                <Route path="tabladevocional" element={<TablaDevocional />} />
-                <Route path="tabladevocional/:id" element={<DevoAd/>} />
-                <Route path="tablanews" element={<TablaNews />} />
-                {/* <Route path="/programas" element={<Programs/>} /> */}
-              </Route>
+                    <Route path="noticia/:id" element={<Noticia />} />
+                    <Route path="recursos/ebooks" element={<Ebooks />} />
+                    <Route
+                      path="recursos/devocional-diario"
+                      element={<Devocional />}
+                    />
+                    <Route
+                      path="recursos/cursos-biblicos"
+                      element={<CursosBiblicos />}
+                    />
+                    <Route
+                      path="noticias-y-eventos"
+                      element={<NewsEvents />}
+                    />
+                    <Route path="donate" element={<Donate />} />
+                    <Route path="contactanos" element={<Contact />} />
+                    <Route path="oracion" element={<Oracion />} />
+                    <Route
+                      path="recursos/devocionales"
+                      element={<DevocionalesDiarios />}
+                    />
+                  </Route>
 
-            </Routes>
-          </BrowserRouter>
-          <Fotter />
-        </Suspense>
-        </div>
+                  <Route path="*" element={<NotFound />} />
+                  <Route path="/admin" element={<Administracion />}>
+                    <Route path="register" element={<RegisterAdmin />} />
+                    <Route path="devoAd" element={<DevoAd />} />
+                    <Route path="login" element={<LoginAdmin />} />
+                    <Route path="newsad" element={<NewsAdmin />} />
+                    <Route path="eventad" element={<EventsAdmin />} />
+                    <Route path="tablaevento" element={<TablaEvento />} />
+                    <Route path="tablaevento/:id" element={<EventsAdmin />} />
+                    <Route
+                      path="tabladevocional"
+                      element={<TablaDevocional />}
+                    />
+                    <Route path="tabladevocional/:id" element={<DevoAd />} />
+                    <Route path="tablanews" element={<TablaNews />} />
+                    {/* <Route path="/programas" element={<Programs/>} /> */}
+                  </Route>
+                </Routes>
+              </BrowserRouter>
+              {/* <Fotter /> */}
+            </Suspense>
+          </div>
         </AuthProvider>
       </StrictMode>
     </>
--- src/pages/client/components/Navbar/index.jsx
+++ src/pages/client/components/Navbar/index.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 import OriginalLogo from "../../../../assets/OriginalLogo.png";
+import {Outlet} from "react-router-dom";
+import WhiteIcon from "../../../../assets/WhiteIcon.png";
 // Importar los iconos de react-icons
 import {
   FaHome,
@@ -417,6 +419,15 @@ const Navbar = () => {
           </li>
         </ul>
       </div>
+      <Outlet/>
+      <div className="flex md:flex-col bg-[#222126] text-white justify-center py-10">
+            <div className="items-center flex flex-col gap-4">
+                <img src={WhiteIcon} alt="qhite-icon" className="w-12 xl:w-16"/>
+                <span className="font-light text-[1.25em] leading-[1.5em] max-[1100px]:text-[1.125em] max-[1100px]:leading-[1.2em]">Cristo para Todas las Naciones - Perú</span>
+            </div>
+            
+
+        </div>
     </>
   );
 };
