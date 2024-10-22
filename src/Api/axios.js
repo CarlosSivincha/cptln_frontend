7dea78c57ab769999411a8c5c388399944235c6f
mvillanueva24 <mariorvillanueva24@gmail.com>
2024-10-22 14:13:43 -0500

    push

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 const instance = axios.create({
     baseURL: import.meta.env.VITE_BACKEND_URL,
-    headers : {
-        "ngrok-skip-browser-warning": "69420",
-    }
-    // withCredentials: true
+    // headers : {
+    //     "ngrok-skip-browser-warning": "69420",
+    // }
+    withCredentials: true
 })
 export default instance
