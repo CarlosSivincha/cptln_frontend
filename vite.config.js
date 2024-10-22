82a6a865000d56edc203f824a22096ba2269bba3
mvillanueva24 <mariorvillanueva24@gmail.com>
2024-10-22 13:08:23 -0500

    push

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-      'api@': path.resolve(__dirname, './src/api/'),
+      'api@': path.resolve(__dirname, './src/api'),
     },
   }
 });
