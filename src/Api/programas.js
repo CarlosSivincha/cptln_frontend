2bb6803d0813f942317dab5026946e6d0f9b851e
JoseDEV799 <josedev799@gmail.com>
2024-10-27 18:34:05 -0500

    tabla programas

--- src/Api/programas.js
+++ src/Api/programas.js
@@ -2,4 +2,6 @@ import axios from "./axios";
 
 export const obtenerProgramas = () => axios.get("programa")
 
+export const obtenerProgramasPagination = (params) => axios.get('programa/pagination', params)
+
 export const obtenerProgramasPorCategorias = (categoria) => axios.post(`programa/nombre`,categoria)
