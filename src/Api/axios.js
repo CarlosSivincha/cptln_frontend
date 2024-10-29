4129fc1e7cc804f497043a7d1b22bab514911642
JoseDEV799 <josedev799@gmail.com>
2024-10-29 16:02:52 -0500

    Programas

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
     baseURL: import.meta.env.VITE_BACKEND_URL,
-    headers : {
-        "bypass-tunnel-reminder": "PERMISOS",
-    }
-    // withCredentials: true
+    withCredentials: true
 })
 export default instance
