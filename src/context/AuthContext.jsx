a3a33b41d3ec605654d1a7f7551a2b60ae114cf3
mvillanueva24 <mariorvillanueva24@gmail.com>
2024-11-07 03:07:44 -0500

    Context

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -22,22 +22,14 @@ export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [isAuthenticated, setIsAuthenticated] = useState(false);
     const [loading, setLoading] = useState(true);
+    const [errorsAuth , setErrorsAuth] = useState("")
 
     const loginUser = async (user) => {
         try {
             const res = await login(user); // Espera a que la promesa se resuelva
-    
-            // Ahora puedes verificar si el token está presente en la cookie
-            const token = Cookies.get('token');
-            console.log(token);
-            
-            if (token) {
-                setUser(res.data);
-                setIsAuthenticated(true);
-            } else {
-                throw new Error('No se recibió el token');
-            }
-            return res
+            setIsAuthenticated(true)
+            setUser(res.data)
+            navigate('admin/')
         } catch (error) {
             console.error('Error en el login:', error.message);
         }
@@ -48,69 +40,61 @@ export const AuthProvider = ({ children }) => {
             const response = await registrar(user)
             console.log(response)
         } catch (error) {
-            console.log(error)
+            if (error.response && error.response.data && error.response.data.errors) {
+                const firstError = error.response.data.errors[0]; // Primer error de la respuesta
+                setErrorsAuth(firstError.message); // Establecer el primer error como estado
+            } else {
+                setErrorsAuth("Error desconocido"); // En caso de que no se obtenga un error esperado
+            }
+    
         }
     }
 
     const LogoutUser = async () => {
         try {
-            const token = Cookies.get('token');
+            const token = localStorage.getItem('token') || Cookies.get('token');
             const res = logout(token)
             if (res) {
                 setIsAuthenticated(false)
                 setUser(null)
-                navigate('/')
+                
             }
         } catch (error) {
-            
+            setErrorsAuth(error)
         }
     }
-
-    const checkLogin = async (token) => {
-        try {
-            const res = await verifyTokenRequest(token);
-            if (!res.data) {
-                setIsAuthenticated(false);
-                setUser(null);
-                return false;
-            } else {
-                setUser(res.data);
-                setIsAuthenticated(true);
-                return true;
-            }
-        } catch (error) {
-            console.error('Token verification error:', error);
-            setIsAuthenticated(false);
-            setUser(null);
-            return false;
-        } finally {
-            setLoading(false);
-        }
-    };
-
 
     const hasRedirected = useRef(false); // Usamos un ref para evitar redirecciones múltiples
     // Verificación inicial al cargar
     useEffect(() => {
-        const initializeAuth = async () => {
-            const token = Cookies.get('token');
-
-            if (!token && location.pathname.includes('/admin') && !hasRedirected.current) {
-                hasRedirected.current = true;  // Marcamos que ya hemos redirigido
-                setIsAuthenticated(false);
+        const checkLogin = async () => {
+            try {
+                const res = await verifyTokenRequest()
+                if (!res.data) {
+                    if (location.pathname.includes('/admin') && !hasRedirected.current) {
+                        hasRedirected.current = true;  // Marcamos que ya hemos redirigido
+                        setIsAuthenticated(false);
+                        setLoading(false);
+                        navigate('/cptln/pe/admin/login');
+                        return;
+                    }
+                } else {
+                    setIsAuthenticated(true);
+                    setUser(res.data);
+                }
                 setLoading(false);
-                navigate('/cptln/pe/admin/login');
-                return;
+            } catch (error) {
+                console.error('Token verification error:', error);
+                setIsAuthenticated(false);
+            } finally {
+                setLoading(false); // Asegúrate de que la carga se complete
             }
-
-            await checkLogin(token);
-        };
-
-        initializeAuth();
+        }
+        checkLogin();
     }, [location]);
 
     return (
-        <AuthContext.Provider value={{ user, loginUser, RegisterUser, LogoutUser, isAuthenticated, loading }} >
+        <AuthContext.Provider value={{ user, loginUser, RegisterUser, LogoutUser, isAuthenticated, loading, errorsAuth }} >
             {children}
         </AuthContext.Provider>
     )
